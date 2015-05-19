55657ce04262bb6f548629d387a7ea86964bfebe
Newton Truong <newton@deep6analytics.com>
2015-05-19 15:43:45 -0700

    incorporated uima type system

---
+++ kite-morphlines/kite-morphlines-cas/src/main/java/org/kitesdk/morphline/cas/ReadCasBuilder.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2015 Qurius Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.kitesdk.morphline.cas;
+
+import com.typesafe.config.Config;
+import org.kitesdk.morphline.api.Command;
+import org.kitesdk.morphline.api.CommandBuilder;
+import org.kitesdk.morphline.api.MorphlineContext;
+import org.kitesdk.morphline.api.Record;
+import org.kitesdk.morphline.stdio.AbstractParser;
+
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * Command that parses an InputStream that contains CAS data; for each CAS object in the stream,
+ * the command emits a morphline record containing the object as an attachment in
+ */
+public final class ReadCasBuilder implements CommandBuilder {
+
+    /** The MIME type identifier that will be filled into output records */
+    public static final String MIME_TYPE = "cas";
+
+    @Override
+    public Collection<String> getNames() {
+        return Collections.singletonList("readCas");
+    }
+
+    @Override
+    public Command build(Config config, Command parent, Command child, MorphlineContext context) {
+        return new ReadCas(this, config, parent, child, context);
+    }
+
+    ///////////////////////////////////////////////////////////////////////////////
+    // Nested classes:
+    ///////////////////////////////////////////////////////////////////////////////
+    private static final class ReadCas extends AbstractParser {
+
+        public ReadCas (CommandBuilder builder, Config config, Command parent, Command child, MorphlineContext context) {
+            super(builder, config, parent, child, context);
+        }
+
+        @Override
+        protected boolean doProcess(Record inputRecord, InputStream in) {
+            System.out.println("inside readcas doprocess");
+
+            return true;
+        }
+    }
+
+}
--- kite-morphlines/kite-morphlines-cas/src/test/java/CasMorphlineTest.java
+++ kite-morphlines/kite-morphlines-cas/src/test/java/CasMorphlineTest.java
@@ -1,10 +1,32 @@
+/*
+ * Copyright 2015 Qurius Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import org.apache.uima.TokenAnnotation;
 import org.apache.uima.UIMAException;
+import org.apache.uima.UIMAFramework;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.annotator.WhitespaceTokenizer;
 import org.apache.uima.cas.admin.CASMgr;
 import org.apache.uima.cas.impl.CASCompleteSerializer;
 import org.apache.uima.cas.impl.Serialization;
+import org.apache.uima.fit.factory.TypeSystemDescriptionFactory;
+import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.jcas.JCas;
-import org.apache.uima.jcas.tcas.Annotation;
+import org.apache.uima.resource.metadata.TypeSystemDescription;
 import org.junit.Test;
 import org.kitesdk.morphline.api.AbstractMorphlineTest;
 import org.kitesdk.morphline.api.Record;
@@ -20,28 +42,18 @@ public class CasMorphlineTest extends AbstractMorphlineTest {
 
     // Create byte buffer that holds the a serialized test CAS Object
     public static byte[] testByteArrayFactory() throws UIMAException, IOException {
-        String testString = "I like to travel through the Ether";
+        String testString = "I like to travel through the Ether.";
 
+        //TypeSystemDescription tsd = TypeSystemDescriptionFactory.createTypeSystemDescription("org.apache.uima.TokenAnnotation", "org.apache.uima.SentenceAnnotation");
+        TypeSystemDescription tsd = TypeSystemDescriptionFactory.createTypeSystemDescription();
         JCas jCas = JCasFactory.createJCas();
         jCas.setDocumentText(testString);
         jCas.setDocumentLanguage("en");
         jCas.createView("docId").setDocumentText("12345");
 
-        Annotation ann = new Annotation(jCas);
-        // set "I"
-        ann.setBegin(0);
-        ann.setEnd(1);
-        ann.addToIndexes();
-        // set "travel"
-        ann = new Annotation(jCas);
-        ann.setBegin(10);
-        ann.setEnd(16);
-        ann.addToIndexes();
-        // set "Ether"
-        ann = new Annotation(jCas);
-        ann.setBegin(29);
-        ann.setEnd(34);
-        ann.addToIndexes();
+        AnalysisEngineDescription aed = AnalysisEngineFactory.createEngineDescription(WhitespaceTokenizer.class);
+        AnalysisEngine ae = UIMAFramework.produceAnalysisEngine(aed);
+        ae.process(jCas);
 
         CASCompleteSerializer casCompleteSerializer = Serialization.serializeCASComplete(jCas.getCasImpl());
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
@@ -54,22 +66,23 @@ public class CasMorphlineTest extends AbstractMorphlineTest {
 
     @Test
     public void testReadCas() throws Exception {
-        morphline = createMorphline("test-morphlines/readJson");
-        for (int i = 0; i < 3; i++) {
-            ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(testByteArrayFactory());
-            ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
-            Record record = new Record();
-            record.put(Fields.ATTACHMENT_BODY, objectInputStream);
+        morphline = createMorphline("test-morphlines/readCas");
 
-            collector.reset();
-            startSession();
-            assertEquals(1, collector.getNumStartEvents());
-            assertTrue(morphline.process(record));
-            Iterator<Record> iter = collector.getRecords().iterator();
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(testByteArrayFactory());
+        ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
+        Record record = new Record();
+        record.put(Fields.ATTACHMENT_BODY, objectInputStream);
 
-            objectInputStream.close();
-        }
+        collector.reset();
+        startSession();
+        assertEquals(1, collector.getNumStartEvents());
+        assertTrue(morphline.process(record));
+        Iterator<Record> iter = collector.getRecords().iterator();
+
+        objectInputStream.close();
     }
 
     @Test
+    public void test() {
+    }
 }
